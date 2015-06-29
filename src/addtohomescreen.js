9c5486c2bb8dc41b6a71930905b00341d394aab4
Laszlo Szikszai <Laszlo_Szikszai@epam.com>
2015-06-29 09:27:37 +0200

    - Fix typo

--- src/addtohomescreen.js
+++ src/addtohomescreen.js
@@ -485,7 +485,7 @@ ath.Class.prototype = {
 
 		// add the action icon
 		message = '<p>' + message.replace(/%icon(?:\[([^\]]+)\])?/gi, function(matches, group1) {
-			return '<span class="ath-action-icon">' + (!!group1 ? group1 : 'icon') + '</span>'
+			return '<span class="ath-action-icon">' + (!!group1 ? group1 : 'icon') + '</span>';
 		}); + '</p>';
 
 		// create the message container
