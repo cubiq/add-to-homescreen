229cb6e93537d1d61503873be7c7388649980081
Jackey Cheung <eidng8@users.noreply.github.com>
2015-07-06 11:45:14 +0800

    minor change on translation
    Just a fix on the wording differences in simplified Chinese.

--- src/addtohomescreen.js
+++ src/addtohomescreen.js
@@ -119,7 +119,7 @@ ath.intl = {
 	},
 
 	zh_cn: {
-		ios: '如要把应用程式加至主屏幕,请点击%icon, 然后<strong>加至主屏幕</strong>',
+		ios: '如要把应用程序加至主屏幕,请点击%icon, 然后<strong>添加到主屏幕</strong>',
 		android: 'To add this web app to the home screen open the browser option menu and tap on <strong>Add to homescreen</strong>. <small>The menu can be accessed by pressing the menu hardware button if your device has one, or by tapping the top right menu icon <span class="ath-action-icon">icon</span>.</small>',
 	},
 
