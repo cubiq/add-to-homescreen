c77c5ca3880f1173058a3b67aba00ffd623711cd
Andy Schroeder <schroe2a@gmail.com>
2017-01-16 16:41:59 -0600

    added check for chrome:  "display-mode: standalone"

--- src/addtohomescreen.js
+++ src/addtohomescreen.js
@@ -212,7 +212,7 @@ ath.OS = ath.isIDevice ? 'ios' : ath.isMobileChrome ? 'android' : ath.isMobileIE
 ath.OSVersion = _ua.match(/(OS|Android) (\d+[_\.]\d+)/);
 ath.OSVersion = ath.OSVersion && ath.OSVersion[2] ? +ath.OSVersion[2].replace('_', '.') : 0;
 
-ath.isStandalone = 'standalone' in window.navigator && window.navigator.standalone;
+ath.isStandalone = ('standalone' in window.navigator && window.navigator.standalone || (window.matchMedia('(display-mode: standalone)').matches));
 ath.isTablet = (ath.isMobileSafari && _ua.indexOf('iPad') > -1) || (ath.isMobileChrome && _ua.indexOf('Mobile') < 0);
 
 ath.isCompatible = (ath.isMobileSafari && ath.OSVersion >= 6) || ath.isMobileChrome;	// TODO: add winphone
